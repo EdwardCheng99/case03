f7104f973efa1bc73895ed7c0972b1152d124307
Edward <edward940715@gmail.com>
2024-09-20 11:45:37 +0800

    測試測試commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // 因為網路上找的用npm裝入的uniq模組是十年前的，比較舊，用CJS寫法
 // https://www.npmjs.com/search?q=uniq
 // npm i uniq 安裝uniq模組
-// npm i 如果不小心將安裝的模組套件檔案刪除了，使用此則會全數裝回
+// npm i 如果不小心將安裝的模組套件檔案刪除了，使用此則會全數裝回1
 const uniq = require("uniq");
 const ary = [1, 3, 3, 5, 7, 9, 9, 9, 9];
 const result = uniq(ary);
